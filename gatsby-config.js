8186d04a6aa744b33c1e46c2234c90e8427a1aee
Matt Bruton <the.bruton@gmail.com>
2018-05-09 23:29:46 -0500

    modified config to include google font plugin, placeholder styling for navigation

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,14 @@ module.exports = {
         name: 'posts',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Raleway\:400,700`,
+          // `source sans pro\:300,400,400i,700` you can also specify font weights and styles
+        ],
+      },
+    },
   ],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,70 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const HeaderLink = styled(Link)`
+  color: #333;
+  font-family: 'Raleway', monospace;
+  text-decoration: none;
+`
+
+const HeaderPrimaryLink = styled(HeaderLink)`
+  font-weight: 700;
+`
+
+const HeaderNavLink = styled(HeaderLink)`
+  font-weight: 400;
+  &:hover {
+    font-weight: 700;
+  }
+`
+const HeaderNavigationList = styled('ul')`
+  align-items: center;
+  display: flex;
+  margin: 0;
+  & li {
+    margin: 0;
+    padding: 0px 0.5rem;
+    list-style: none;
+  }
+  & li:nth-child(1) {
+    padding: 0 0.5rem 0 0;
+  }
+`
 
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: 'white',
       marginBottom: '1.45rem',
+      borderBottom: '.2rem solid #333333',
     }}
   >
     <div
       style={{
+        display: 'flex',
+        justifyContent: 'space-between',
         margin: '0 auto',
-        maxWidth: 960,
+        maxWidth: 1376,
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
+      <h1 style={{ margin: 0, fontSize: '1.75em' }}>
+        <HeaderPrimaryLink to="/">{siteTitle}</HeaderPrimaryLink>
       </h1>
+      <nav style={{ display: 'flex', alignItems: 'center' }}>
+        <HeaderNavigationList>
+          <li>
+            <HeaderNavLink to="/blog">Blog</HeaderNavLink>
+          </li>
+          <li>
+            <HeaderNavLink to="/contact">Contact</HeaderNavLink>
+          </li>
+          <li>
+            <HeaderNavLink to="/about">About Me</HeaderNavLink>
+          </li>
+        </HeaderNavigationList>
+      </nav>
     </div>
   </div>
 )
